228e6125fdf863e7f0d581e791f16042ce09e167
cloden00 <yufusawa@outlook.com>
2026-08-04 12:32:06 +0300

    assert price greater than zero

--- test_supplier.py
+++ test_supplier.py
@@ -1,7 +1,16 @@
 import requests
+import random
 
 def test_supplier_api_check():
-    r = requests.get('https://fakestoreapi.com/products/1')
+    response = requests.get('https://fakestoreapi.com/products/1')
 
-    assert r.status_code == 200 
+    assert response.status_code == 200 
 
+    product = response.json()
+
+    # randomizer = random.randint(-150, 150)
+    # product['price'] = randomizer
+
+    print (f"\nPRICE = {product['price']}")
+
+    assert product['price'] > 0 
